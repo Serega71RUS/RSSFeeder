929382074029dae3a5438bb72bfd6b6f52587646
Serega71RUS <serega71rus@gmail.com>
2021-10-07 13:03:16 +0300

    Добавьте файлы проекта.

---
+++ RSSFeeder/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeHollow.FeedReader;
+
+namespace RSSFeeder.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public static IList<FeedItem> allfeed;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string url = "https://habr.com/ru/rss/interesting/";
+            var urlsTask = FeedReader.GetFeedUrlsFromUrlAsync(url);
+            var urls = urlsTask.Result;
+            string feedUrl = url;
+            var readerTask = FeedReader.ReadAsync(feedUrl);
+            readerTask.ConfigureAwait(false);
+            allfeed = readerTask.Result.Items;
+            foreach (var item in readerTask.Result.Items)
+            {
+                Console.WriteLine(item.Title + " - " + item.Link);
+            }
+        }
+    }
+}
---
+++ RSSFeeder/Pages/Settings.cshtml.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Xml;
+using System.Net;
+
+namespace RSSFeeder.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public string Check { get; set; }
+        //private readonly decimal currentRate = 64.1m;
+
+        public string url { get; set; }
+        public int timeS { get; set; }
+
+        private static Timer aTimer;
+
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = "Сохранить";
+            ReadXML();
+            //SetTimer();
+            
+        }
+        public void OnPost(string urltextbox, int timetextbox)
+        {
+            
+            WriteXML(urltextbox, timetextbox);
+            ReadXML();
+        }
+
+        public void ReadXML()
+        {
+            //Message = "Ссылка подходит";
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Settings.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Name == "url")
+                    url = xnode.InnerText;
+                if (xnode.Name == "updatetime")
+                    timeS = Convert.ToInt32(xnode.InnerText);
+            }
+        }
+
+        public void WriteXML(string url, int time)
+        {
+            Uri urlCheck = new Uri(url);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlCheck);
+            request.Timeout = 5000;
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    XmlDocument xDoc = new XmlDocument();
+                    xDoc.Load("Settings.xml");
+                    XmlElement xRoot = xDoc.DocumentElement;
+                    xRoot.RemoveAll();
+                    xDoc.Save("Settings.xml");
+                    //XmlElement configRSSElement = xDoc.CreateElement("configRSS");
+                    XmlElement urlElem = xDoc.CreateElement("url");
+                    XmlElement updatetimeElem = xDoc.CreateElement("updatetime");
+                    XmlText urlText = xDoc.CreateTextNode(url);
+                    XmlText updatetimeText = xDoc.CreateTextNode(Convert.ToString(time));
+
+                    urlElem.AppendChild(urlText);
+                    updatetimeElem.AppendChild(updatetimeText);
+                    xRoot.AppendChild(urlElem);
+                    xRoot.AppendChild(updatetimeElem);
+                    //xRoot.AppendChild(configRSSElement);
+                    xDoc.Save("Settings.xml");
+                    Message = "Сохранено";
+                }
+            }
+            catch (Exception)
+            {
+                Message = "Неподходящая ссылка";
+            }
+        }
+    }
+}
